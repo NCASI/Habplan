d3c912d07874c4099c6d83bbb6f14f2ff5270df6
maxdoltonjones2 <153532476+maxdoltonjones2@users.noreply.github.com>
2024-01-31 14:04:43 -0500

    Update ComPlot_func_v1.R
    Fixing flow column heading selection issue

--- HabplanR/R/ComPlot_func_v1.R
+++ HabplanR/R/ComPlot_func_v1.R
@@ -20,27 +20,23 @@
 
 comPlot <- function(flow.data.1, flow.data.2, flow.data.3,
                     nyear){
-  
+
   flow.list.1 <- vector(mode = "list", length = nyear)
-  
+
   #First flow here
-  #Format flow file to remove commas and extra column
-  for (i in 1:ncol(flow.data.1)) {
-    flow.data.1[,i] <- gsub(",", "", flow.data.1[,i])
-  }
-  flow1 <- flow.data.1[,-1]
-  
+  flow1 <- flow.data.1
+
   for (i in 1:length(flow.list.1)) {
-    std <- cbind(flow1[1], flow1[i+2], flow1[i+37])
+    std <- cbind(flow1[2], flow1[i+3], flow1[i+(nyear+2)])
     colnames(std) <- c("id", "year", "flow")
-    #std$flow <- gsub('[,]', '', std$flow)
-    #std$year <- gsub('[,]', '', std$year)
-    #std$id <- gsub('[,]', '', std$id)
+    std$flow <- gsub('[,]', '', std$flow)
+    std$year <- gsub('[,]', '', std$year)
+    std$id <- gsub('[,]', '', std$id)
     std$flow <- as.numeric(std$flow)
     tot.flow <- sum(std$flow)
     flow.list.1[i] <- tot.flow
   }
-  
+
   year <- c(1:nyear)
   flow <- t(flow.list.1)
   flow <- t(flow)
@@ -50,23 +46,22 @@ comPlot <- function(flow.data.1, flow.data.2, flow.data.3,
   f1.output$flow <- as.numeric(f1.output$flow)
   f1.output$year <- as.numeric(f1.output$year)
   f1.output$num <- "flow 1"
-  
+
   #Second flow here
   flow.list.2 <- vector(mode = "list", length = nyear)
-  #Format flow file to remove commas and extra column
-  for (i in 1:ncol(flow.data.2)) {
-    flow.data.2[,i] <- gsub(",", "", flow.data.2[,i])
-  }
-  flow1 <- flow.data.2[,-1]
-  
+  flow1 <- flow.data.2
+
   for (i in 1:length(flow.list.2)) {
-    std <- cbind(flow1[1], flow1[i+2], flow1[i+37])
+    std <- cbind(flow1[2], flow1[i+3], flow1[i+(nyear+2)])
     colnames(std) <- c("id", "year", "flow")
+    std$flow <- gsub('[,]', '', std$flow)
+    std$year <- gsub('[,]', '', std$year)
+    std$id <- gsub('[,]', '', std$id)
     std$flow <- as.numeric(std$flow)
     tot.flow <- sum(std$flow)
     flow.list.2[i] <- tot.flow
   }
-  
+
   year <- c(1:nyear)
   flow <- t(flow.list.2)
   flow <- t(flow)
@@ -76,23 +71,22 @@ comPlot <- function(flow.data.1, flow.data.2, flow.data.3,
   f2.output$flow <- as.numeric(f2.output$flow)
   f2.output$year <- as.numeric(f2.output$year)
   f2.output$num <- "flow 2"
-  
+
   #Third flow here
   flow.list.3 <- vector(mode = "list", length = nyear)
-  #Format flow file to remove commas and extra column
-  for (i in 1:ncol(flow.data.3)) {
-    flow.data.3[,i] <- gsub(",", "", flow.data.3[,i])
-  }
-  flow1 <- flow.data.3[,-1]
-  
+  flow1 <- flow.data.3
+
   for (i in 1:length(flow.list.3)) {
-    std <- cbind(flow1[1], flow1[i+2], flow1[i+37])
+    std <- cbind(flow1[2], flow1[i+3], flow1[i+(nyear+2)])
     colnames(std) <- c("id", "year", "flow")
+    std$flow <- gsub('[,]', '', std$flow)
+    std$year <- gsub('[,]', '', std$year)
+    std$id <- gsub('[,]', '', std$id)
     std$flow <- as.numeric(std$flow)
     tot.flow <- sum(std$flow)
     flow.list.3[i] <- tot.flow
   }
-  
+
   year <- c(1:nyear)
   flow <- t(flow.list.3)
   flow <- t(flow)
@@ -102,29 +96,32 @@ comPlot <- function(flow.data.1, flow.data.2, flow.data.3,
   f3.output$flow <- as.numeric(f3.output$flow)
   f3.output$year <- as.numeric(f3.output$year)
   f3.output$num <- "flow 3"
-  
+
   all.output <- rbind(f1.output, f2.output, f3.output)
-  
+
   #library(ggplot2)
   f1.plot <- ggplot(data = all.output) +
     geom_line(data = all.output, aes(x = year, y = flow, group = num, color = num),
               position = "identity") +
     geom_area(data = all.output, aes(x = year, y = flow, group = num, fill = num),
               alpha = 0.5, position = "identity") +
+    #ylim(0, 10000) +
+    #geom_hline(yintercept = target, linetype = 2, color = "red") +
+    #geom_hline(yintercept = (target+th.hi), linetype = 2, color = "black") +
+    #geom_hline(yintercept = (target-th.lo), linetype = 2, color = "black") +
     scale_color_viridis_d(option = "viridis", begin = 0.3, end = 0.6) +
     scale_fill_viridis_d(option = "viridis", begin = 0.3, end = 0.6) +
-    scale_x_continuous(breaks=seq(0,nyear,5), expand = c(0, 0)) +
-    scale_y_continuous(expand = c(0, 0)) +
+    scale_x_continuous(breaks=seq(0,nyear,5)) +
     ggtitle("Flow output over time") +
     xlab("Year") +
     ylab("Flow") +
     theme_classic() +
-    guides(color = guide_legend(title = "Flow #"), fill = guide_legend(title = "Flow #")) +
-  theme(text = element_text(size=18))
-  
-  ggsave(file = paste0("./Combined_flows.png"), width = 200, height = 120,
+    guides(color = guide_legend(title = "Flow #"), fill = guide_legend(title = "Flow #"))
+  #theme(legend.position = "none")
+
+  ggsave(file = paste0("./example/RCW/RCW/Outputs/Combined_flows.png"), width = 200, height = 120,
          dpi = 600, units = "mm")
-  
+
   return(f1.plot)
 }
 
